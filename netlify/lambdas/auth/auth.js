8c5b2eedd57af4e3740491409a4fd0b0ff77c196
Sylvain METAYER <contact@sylvainmetayer.fr>
2020-01-11 23:54:28 +0100

    update

--- netlify/lambdas/auth/auth.js
+++ netlify/lambdas/auth/auth.js
@@ -1,14 +1,10 @@
 require('dotenv').config()
 const jwt = require("jsonwebtoken");
 
-console.log(process.cwd())
-
 exports.handler = function (event, context, callback) {
   if (event.httpMethod != "POST") {
     return callback(null, { statusCode: 405, body: "Only POST authorized" });
   }
-
-  console.log(process.env.LOGIN_PASSWORD);
 
   const body = JSON.parse(event.body);
 
@@ -16,9 +12,18 @@ exports.handler = function (event, context, callback) {
     return callback(null, { statusCode: 401, body: "Bad credentials" });
   }
 
+  const token = jwt.sign({ logged: true }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
   // Valid password ! Let's generate a JWT token
   callback(null, {
-    statusCode: 401,
-    body: "Hello, World " + process.env.API_KEY
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "access_token": token,
+      "token_type": "JWT",
+      "expires_in": 3600
+    })
   });
 }
