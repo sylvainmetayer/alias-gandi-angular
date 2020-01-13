e60ab9928addceced02e74019ff38f1c5f4d251c
Sylvain METAYER <s.metayer@groupeonepoint.com>
2020-01-13 14:03:34 +0100

    login service OK

--- src/app/auth/login.service.ts
+++ src/app/auth/login.service.ts
@@ -1,41 +1,80 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { LoginResponse } from './login-response';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+
+export interface LoginResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
   private BASE_URL = '/api';
 
-  constructor(private client: HttpClient) { }
+  constructor(private client: HttpClient) {}
+
+  isConnected: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  private token: string = null;
 
   isLoggedIn(): Observable<boolean> {
     return of(false);
   }
 
-  login(password: string): Observable<LoginResponse> {
+  login(password: string): Observable<boolean> {
+    const loginObserver = new Subject<boolean>();
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: 'my-auth-token'
+        'Content-Type': 'application/json'
       })
     };
 
-    return this.client.post<LoginResponse>(this.BASE_URL + '/auth', {
-      password: password
-    }).pipe(response => {
-      // if(!response.ok) {
-      //   return false;
-      // }
-      // localStorage.setItem('token', response.access_token)
-      // return true;
-      // console.log(response);
-      return response;
-    });
+    this.client
+      .post<LoginResponse>(
+        this.BASE_URL + '/auth',
+        {
+          password
+        },
+        httpOptions
+      )
+      .subscribe(
+        data => {
+          localStorage.setItem('token', data.access_token);
+          this.token = data.access_token;
+          loginObserver.next(true);
+        },
+        (err: HttpErrorResponse) => {
+          console.log(err);
+          loginObserver.next(false);
+        },
+        () => {
+          loginObserver.complete();
+        }
+      );
 
+    return loginObserver;
+  }
+
+  logout(): void {
+    this.isConnected.next(false);
+    localStorage.removeItem('token');
+  }
+
+  getToken(): string {
+    return this.token;
+  }
+
+  getAuthTokenPromise(): Promise<string | Error> {
+    return new Promise((resolve, reject) => {
+      this.token ? resolve(this.token) : reject();
+    });
   }
 }
